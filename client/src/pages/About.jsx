1ea3238c3786f846b1c8a94cb2f4b1573ef1b642
Askebir <askebirtadesse@gmail.com>
2024-05-25 04:36:52 -0700

    complete home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+    <h1 className='text-3xl font-bold mb-4 text-slate-800' >About</h1>
+    <p className="mb-4 text-slate-700">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+      error! Ipsam dolorem repellendus porro debitis maiores quisquam, nemo
+      dolor commodi molestiae et placeat veritatis vitae excepturi quasi
+      provident ullam a.
+    </p>
+    <p className="mb-4 text-slate-700">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+      error! Ipsam dolorem repellendus porro debitis maiores quisquam, nemo
+      dolor commodi molestiae et placeat veritatis vitae excepturi quasi
+      provident ullam a.
+    </p>
+    <p className="mb-4 text-slate-700">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+      error! Ipsam dolorem repellendus porro debitis maiores quisquam, nemo
+      dolor commodi molestiae et placeat veritatis vitae excepturi quasi
+      provident ullam a.
+    </p>
+  </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className='text-3xl font-bold mb-4 text-slate-800' >Welcom to my Auth App</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+        error! Ipsam dolorem repellendus porro debitis maiores quisquam, nemo
+        dolor commodi molestiae et placeat veritatis vitae excepturi quasi
+        provident ullam a.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+        error! Ipsam dolorem repellendus porro debitis maiores quisquam, nemo
+        dolor commodi molestiae et placeat veritatis vitae excepturi quasi
+        provident ullam a.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+        error! Ipsam dolorem repellendus porro debitis maiores quisquam, nemo
+        dolor commodi molestiae et placeat veritatis vitae excepturi quasi
+        provident ullam a.
+      </p>
+    </div>
+  );
 }
